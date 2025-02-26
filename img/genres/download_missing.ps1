8e9f7ccd558bd3d947f768f3986a52227fdbcbcf
Justin322322 <jmrsibonga@bpsu.edu.ph>
2025-02-27 03:26:05 +0800

    update

--- img/genres/download_missing.ps1
+++ img/genres/download_missing.ps1
@@ -8,14 +8,14 @@ function Download-Image {
     )
     
     Write-Host "Downloading $outputFile from $url"
-    Invoke-WebRequest -Uri $url -OutFile $outputFile -UserAgent "Mozilla/5.0"
+    Invoke-WebRequest -Uri $url -OutFile $outputFile -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     Write-Host "Downloaded $outputFile successfully"
 }
 
 # Create array of image details [url, filename]
 $images = @(
     # Hip Hop image - alternate source
-    @("https://cdn.pixabay.com/photo/2016/11/18/13/47/band-1834668_1280.jpg", "hiphop.jpg"),
+    @("https://images.pexels.com/photos/1309240/pexels-photo-1309240.jpeg", "hiphop.jpg"),
     
     # Classical music - alternate source
     @("https://cdn.pixabay.com/photo/2014/05/21/15/18/musician-349790_1280.jpg", "classical.jpg")
